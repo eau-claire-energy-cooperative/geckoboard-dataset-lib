db155c496ec06596f2184c8d0b9ce1952ceecd53
Rob Weber <rweber@ecec.com>
2018-01-17 08:37:07 -0600

    get rid of imports we don't need

--- src/main/java/com/ecec/rweber/geckoboard/model/DataRow.java
+++ src/main/java/com/ecec/rweber/geckoboard/model/DataRow.java
@@ -1,12 +1,8 @@
 package com.ecec.rweber.geckoboard.model;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class DataRow {
 	@JsonIgnore
@@ -14,6 +10,16 @@ public class DataRow {
 	
 	public DataRow(){
 		m_data = new HashMap<String,Object>();
+	}
+	
+	public void addData(String column, double value){
+		//we know it's a doube already, just add it
+		m_data.put(column, value);
+	}
+	
+	public void addData(String column, int value){
+		//we know it's an int already, just add it
+		m_data.put(column, value);
 	}
 	
 	public void addData(DataType type, String column, String value){
@@ -38,25 +44,5 @@ public class DataRow {
 	@JsonAnyGetter
 	public Map<String,Object> getData(){
 		return m_data;
-	}
-	
-	public static void main(String[] args){
-		List<DataRow> rows = new ArrayList<DataRow>();
-		
-		DataRow one = new DataRow();
-		one.addData(DataType.NUMBER, "test", "55");
-		one.addData(DataType.STRING, "name", "test");
-		rows.add(one);
-		
-		ObjectMapper mapper = new ObjectMapper();
-	    String result;
-		try {
-			result = mapper.writeValueAsString(rows);
-		    System.out.println(result);
-		} catch (JsonProcessingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
 	}
 }
