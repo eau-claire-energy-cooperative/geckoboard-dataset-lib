03cfcbbde2db1a3b6d0a227e13c4fa42fdfadaa8
Rob Weber <rweber@ecec.com>
2018-01-17 08:35:08 -0600

    datarow should contain a map, not be one

--- src/main/java/com/ecec/rweber/geckoboard/model/DataRow.java
+++ src/main/java/com/ecec/rweber/geckoboard/model/DataRow.java
@@ -1,16 +1,62 @@
 package com.ecec.rweber.geckoboard.model;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class DataRow extends HashMap<String,Object>{
-	private static final long serialVersionUID = -9126028571183555258L;
-
+public class DataRow {
+	@JsonIgnore
+	private Map<String,Object> m_data = null;
+	
+	public DataRow(){
+		m_data = new HashMap<String,Object>();
+	}
+	
 	public void addData(DataType type, String column, String value){
 		
 		if(type == DataType.NUMBER || type == DataType.PERCENTAGE){
-			this.put(column, Double.parseDouble(value));
+			
+			//check if decimal
+			if(value.contains("."))
+			{
+				m_data.put(column, Double.parseDouble(value));
+			}
+			else
+			{
+				m_data.put(column, Integer.parseInt(value));
+			}
 		}
 		else {
-			this.put(column,value);
+			m_data.put(column,value);
 		}
+	}
+	
+	@JsonAnyGetter
+	public Map<String,Object> getData(){
+		return m_data;
+	}
+	
+	public static void main(String[] args){
+		List<DataRow> rows = new ArrayList<DataRow>();
+		
+		DataRow one = new DataRow();
+		one.addData(DataType.NUMBER, "test", "55");
+		one.addData(DataType.STRING, "name", "test");
+		rows.add(one);
+		
+		ObjectMapper mapper = new ObjectMapper();
+	    String result;
+		try {
+			result = mapper.writeValueAsString(rows);
+		    System.out.println(result);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
